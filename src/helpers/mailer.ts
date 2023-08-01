d8a6e4a86b475da9698c2dbdbfb85cb412e4dd9b
Adam West <Adamwest023@gmail.com>
2023-08-01 17:39:05 -0500

    added mailer helper

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,8 @@
+var transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "908d44b46e2fd5",
+      pass: "f7069e8d1f7b1a"
+    }
+  });
