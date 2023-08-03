fba9d7879c72166cafa2ccad53825dcbd7ed2b63
Adam West <Adamwest023@gmail.com>
2023-08-03 17:52:27 -0500

    continued testing for forgot password

--- src/app/api/users/updatepassword/route.ts
+++ src/app/api/users/updatepassword/route.ts
@@ -5,28 +5,33 @@ import User from "@/models/userModel";
 connect()
 
 export async function POST(request: NextRequest) {
+
+
     try {
         const reqBody = await request.json()
         const { token, password } = reqBody
-        console.log(token, password);
+        console.log(reqBody);
+        console.log(token);
+        
+        const user = await User.findOne({forgotPasswordToken: token,forgotPasswordTokenExpiry: {$gt: Date.now()}});
+        
 
-        const user = await User.findOne({ forgotPasswordToken: token, forgotPasswordTokenExpiry: { $gt: Date.now() } });
+        // if (!user) {
+        //     return NextResponse.json({ error: "Invalid token" }, { status: 400 });
+        //     console.log(token);
+        // }
+        console.log(user.token);
+        
 
-        if (!user) {
-            return NextResponse.json({ error: "Invalid token" }, { status: 400 });
-        }
+        // user.password = password;
+        // user.forgotPasswordToken = undefined;
+        // user.forgotPasswordTokenExpiry = undefined;
+        // await user.save();
 
-        console.log(user);
-
-        user.password = password;
-        user.forgotPasswordToken = undefined;
-        user.forgotPasswordTokenExpiry = undefined;
-        await user.save();
-
-        return NextResponse.json({
-            message: "Password Updated",
-            success: true
-        });
+        // return NextResponse.json({
+        //     message: "Password Updated",
+        //     success: true
+        // });
 
     } catch (error: any) {
         return NextResponse.json({ error: error.message }, { status: 506 });
--- src/app/updatepassword/page.tsx
+++ src/app/updatepassword/page.tsx
@@ -4,23 +4,40 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { toast } from "react-hot-toast";
+import { set } from "mongoose";
 
 
 export default function UpdatePasswordPage() {
     const [token, setToken] = useState("");
-    const [user,setUser] = useState({
-        password:""})
+    const [user, setUser] = useState({
+        password: " ",
+        token: " "
+    })
     // const [password, setPassword] = useState(false);
     const [error, setError] = useState(false);
+    const [loading, setLoading] = React.useState(false);
+
+
+
 
     //update password
     const onUpdatePassword = async () => {
+        user.token= token
+        
+        
         try {
-            await axios.post('/api/users/updatepassword',{token});
+            setLoading(true);
+            console.log(user.password);
+            console.log(user.token); 
+            const response = await axios.post('/api/users/updatepassword',  user );
+            console.log(response.data);
+
         } catch (error: any) {
             setError(true);
             console.log(error.response.data);
             
+        } finally {
+            setLoading(false);
         }
     }
     //use effect to pull token
@@ -30,20 +47,20 @@ export default function UpdatePasswordPage() {
     }, []);
     //use effect to validate token
     useEffect(() => {
-        if(token.length > 0) {
+        if (token.length > 0) {
             onUpdatePassword();
         }
     }, [token]);
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1 className="text-4xl">Update password</h1>
+            <h1 className="text-4xl">{loading ? "Processing" : "Update password"}</h1>
             <hr />
-            <label htmlFor="password">password</label>
+            <label htmlFor="password">{token ? `${token}` : "no token"}</label>
             <input
                 className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
                 id="password"
-                type="text"
+                type="password"
                 value={user.password}
                 onChange={(e) => setUser({ ...user, password: e.target.value })}
                 placeholder="password"
