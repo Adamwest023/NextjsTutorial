ff3a466c1e691371c9efdfa332d2dba4037e9ff8
Adam West <Adamwest023@gmail.com>
2023-08-10 11:21:36 -0500

    updated verifyemail

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -25,7 +25,7 @@ export default function VerifyEmailPage() {
     useEffect(() => {
         const urlToken = window.location.search.split("=")[1];
         setToken(urlToken || "");
-    }, [setToken]);
+    }, []);
 
 
     useEffect(() => {
